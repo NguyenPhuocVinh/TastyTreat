de043f3c535c23366a717556284b3e64b5480f53
Nguyen Phuoc Vinh <hanggon2003@gmail.com>
2025-01-13 10:05:54 +0700

    @

---
+++ Frontend-TT/build/precache-manifest.52e756a45067ac0866568d1fea1a0385.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f43e982f8e7feb2ab64f0dbe5223f4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "244d14564480e51d2ee2",
+    "url": "/static/css/2.844c7879.chunk.css"
+  },
+  {
+    "revision": "93c29bd473a3fd0905b6",
+    "url": "/static/css/main.41b4992a.chunk.css"
+  },
+  {
+    "revision": "244d14564480e51d2ee2",
+    "url": "/static/js/2.8f4fe15d.chunk.js"
+  },
+  {
+    "revision": "cedbe36c401e63908b9009393e784b42",
+    "url": "/static/js/2.8f4fe15d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93c29bd473a3fd0905b6",
+    "url": "/static/js/main.b4a7ee76.chunk.js"
+  },
+  {
+    "revision": "5b5e3d062a12da101d29",
+    "url": "/static/js/runtime-main.5f93e601.js"
+  },
+  {
+    "revision": "93f0216a91b5d918fdc61c72c5487f46",
+    "url": "/static/media/delivery-guy.93f0216a.png"
+  },
+  {
+    "revision": "3d089af09dd5bfd3631c6d22323a1d4b",
+    "url": "/static/media/fast.3d089af0.png"
+  },
+  {
+    "revision": "a743629d143c34f39498a34cc348e936",
+    "url": "/static/media/google-icon.a743629d.webp"
+  },
+  {
+    "revision": "a11d7af7b05474c3fe3028f7f18ba217",
+    "url": "/static/media/poster.a11d7af7.jpg"
+  },
+  {
+    "revision": "95594d473be3e0b95f18cdc87b64b5df",
+    "url": "/static/media/poster2.95594d47.jpg"
+  },
+  {
+    "revision": "31d28485e1cf7369272270fd730327c0",
+    "url": "/static/media/remixicon.31d28485.eot"
+  },
+  {
+    "revision": "881fbc46361e0c0e5f003c159b2f3005",
+    "url": "/static/media/remixicon.881fbc46.woff"
+  },
+  {
+    "revision": "888e61f04316f10bddfff7bee10c6dd0",
+    "url": "/static/media/remixicon.888e61f0.ttf"
+  },
+  {
+    "revision": "95138f36e015ad912c37db92164f5844",
+    "url": "/static/media/remixicon.95138f36.svg"
+  },
+  {
+    "revision": "9915fef980fa539085da55b84dfde760",
+    "url": "/static/media/remixicon.9915fef9.woff2"
+  },
+  {
+    "revision": "2b82a8a1f41923e2d0741a2fefbb0f29",
+    "url": "/static/media/res-logo.2b82a8a1.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "31323b26fa017addaac2840bce4bf758",
+    "url": "/static/media/vnpayrow.31323b26.png"
+  }
+]);
